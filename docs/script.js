295fbed9348e54641b041a0c68e06efc25ebe1c8
Nguyen Phuc An <phucan.nguyen@hanmail.net>
2024-11-03 17:15:36 +0700

    Revert "Add VERCEL_APP_URL to index.html and update fetch calls in script.js"
    This reverts commit 3b215a6eeaced08aa75d53ef4238694771ffd07d.

--- docs/script.js
+++ docs/script.js
@@ -4,8 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function fetchDevices() {
     console.log('Fetching devices...');
-    const vercelAppUrl = window.VERCEL_APP_URL;
-    fetch(`${vercelAppUrl}/api/devices`)
+    fetch('/api/devices')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok ' + response.statusText);
@@ -83,8 +82,7 @@ function editRow(button) {
 }
 
 function updateDeviceStatus(deviceId, newStatus) {
-    const vercelAppUrl = window.VERCEL_APP_URL;
-    fetch(`${vercelAppUrl}/api/devices?id=${deviceId}`, {
+    fetch(`/api/devices?id=${deviceId}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
